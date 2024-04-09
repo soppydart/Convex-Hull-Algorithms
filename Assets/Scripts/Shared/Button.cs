3956de703ed6da7c152707a79705f46337043061
Pritam <pritambasu14@gmail.com>
2024-04-09 21:08:32 +0530

    remove unneccesary comments from the shared scripts

--- Assets/Scripts/Shared/Button.cs
+++ Assets/Scripts/Shared/Button.cs
@@ -6,13 +6,11 @@ public class HoverCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 {
     public Texture2D hoverCursor;
 
-    // Call this method when the mouse pointer enters the button
     public void OnPointerEnter(PointerEventData eventData)
     {
         Cursor.SetCursor(hoverCursor, Vector2.zero, CursorMode.Auto);
     }
 
-    // Call this method when the mouse pointer exits the button
     public void OnPointerExit(PointerEventData eventData)
     {
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
--- Assets/Scripts/Shared/GridRenderer.cs
+++ Assets/Scripts/Shared/GridRenderer.cs
@@ -15,17 +15,10 @@ public class GridRenderer : MonoBehaviour
 
     void RenderGrid()
     {
-        /*GameObject gridObject = new GameObject("Grid");
-        LineRenderer lineRenderer = gridObject.AddComponent<LineRenderer>();
-        lineRenderer.material = lineMaterial;
-        lineRenderer.startWidth = lineWidth;
-        lineRenderer.endWidth = lineWidth;*/
-
-        // Draw horizontal lines
         for (float y = -gridSize; y <= gridSize; y += gridSpacing)
         {
-            GameObject gridObject = new GameObject("Grid"); // Reset the GameObject for re-use
-            LineRenderer lineRenderer = gridObject.AddComponent<LineRenderer>(); // Reset the LineRenderer for re-use
+            GameObject gridObject = new GameObject("Grid");
+            LineRenderer lineRenderer = gridObject.AddComponent<LineRenderer>();
             lineRenderer.material = lineMaterial;
             lineRenderer.startWidth = ( y == 0)?axisLineWidth:lineWidth;
             lineRenderer.endWidth = (y == 0) ? axisLineWidth : lineWidth;
@@ -35,16 +28,10 @@ public class GridRenderer : MonoBehaviour
             lineRenderer.SetPosition(1, new Vector3(gridSize, y, 0));
         }
 
-        // Draw vertical lines
-        /*gridObject = new GameObject("Grid"); // Reset the GameObject for re-use
-        lineRenderer = gridObject.AddComponent<LineRenderer>(); // Reset the LineRenderer for re-use
-        lineRenderer.material = lineMaterial;
-        lineRenderer.startWidth = lineWidth;
-        lineRenderer.endWidth = lineWidth;*/
         for (float x = -gridSize; x <= gridSize; x += gridSpacing)
         {
-            GameObject gridObject = new GameObject("Grid"); // Reset the GameObject for re-use
-            LineRenderer lineRenderer = gridObject.AddComponent<LineRenderer>(); // Reset the LineRenderer for re-use
+            GameObject gridObject = new GameObject("Grid");
+            LineRenderer lineRenderer = gridObject.AddComponent<LineRenderer>();
             lineRenderer.material = lineMaterial;
             lineRenderer.startWidth = (x == 0) ? axisLineWidth : lineWidth;
             lineRenderer.endWidth = (x == 0) ? axisLineWidth : lineWidth;
